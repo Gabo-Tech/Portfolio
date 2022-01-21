fe4e20eb62f0f111a10535c2903a138c9144245a
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-01-21 18:30:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $(".appBrewery").on("click", function() {
     $(".appBrewery1").slideToggle(slow);
   });
@@ -20,6 +21,9 @@ $(document).ready(function() {
   $(".java").on("click", function() {
     $(".java1").slideToggle(slow);
   });
+
+
+
   $(".button").on("click", function() {
     $("object").slideToggle(slow);
   });
@@ -27,27 +31,44 @@ $(document).ready(function() {
     openMessage: "Full screen",
     closeMessage: "Close full screen"
   });
+
 });
+
 (function($) {
   $.responsiveIframes = function(el, options) {
     var self = this;
+
+    // Access to jQuery and DOM versions of element
     self.$el = $(el);
     self.el = el;
+
+    // Add a reverse reference to the DOM object
     self.$el.data("responsiveIframes", self);
+
     self.init = function() {
       self.options = $.extend({}, $.responsiveIframes.defaultOptions, options);
+
+      // wrap iframe
       var iframeSrc = self.$el.find('iframe').wrap('<div class="iframe-content" />').attr('src');
+
+      //generate header
       var header = '<div class="iframe-header">' +
         '<a href="' + iframeSrc + '" class="iframe-trigger">' + self.options.openMessage + '</a>' +
         '</div>';
+
       var trigger = self.$el.prepend(header).find('.iframe-trigger');
+
+      // click event
       $(trigger).click(function(e) {
         e.preventDefault();
+
         var $this = $(this),
           $html = $('html'),
           isFullScreen = $html.hasClass("iframe-full-screen"),
           message = isFullScreen ? self.options.openMessage : self.options.closeMessage;
+
         $this.text(message);
+
         if (isFullScreen) {
           self.$el.removeClass("iframe-active");
           $html.removeClass("iframe-full-screen");
@@ -59,17 +80,23 @@ $(document).ready(function() {
           self.$el.addClass("iframe-active");
           $html.addClass("iframe-full-screen");
         }
+
       });
     };
+
+    // Run initializer
     self.init();
   };
+
   $.responsiveIframes.defaultOptions = {
     openMessage: "Full screen",
     closeMessage: "Close"
   };
+
   $.fn.responsiveIframes = function(options) {
     return this.each(function() {
       (new $.responsiveIframes(this, options));
     });
   };
+
 })(jQuery);
