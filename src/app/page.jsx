cca45a5317cfd57ef02aff324605d6bb61a7ba44
Gabriel Clemente <clementeaquipresente@yahoo.com>
2025-11-02 20:23:43 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -169,7 +169,7 @@ const Homepage = () => {
               <div className="text-4xl md:text-6xl font-bold z-0">
                 <TypingAnimation texts={texts} />
               </div>
-              {/* DESCRIPTION */}
+              {/* DESCRIPTION 
               <p className="md:text-xl z-0 px-4 sm:px-8 md:px-12 lg:px-0">
                 Welcome to my digital canvas, where innovation and creativity
                 converge. With a keen eye for aesthetics and a mastery of both
@@ -181,7 +181,7 @@ const Homepage = () => {
                 applications. Explore my work and discover the passion and
                 expertise that drive me to deliver exceptional results in every
                 endeavor.
-              </p>
+              </p>*/}
               {/* BUTTONS */}
               <div className="w-full flex flex-col lg:flex-row gap-4 z-0 mb-5 px-4 sm:px-8 md:px-12 lg:px-0">
                 <Link
